f2608702bf4d36a9ee16e5bcba057d108ce62482
Liang Yu Pan <xxzero12@gmail.com>
2020-09-11 02:48:27 -0700

    remove unused test

--- client/App.test.tsx
+++ client/App.test.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import { render } from 'react-native-testing-library'
 import App from './App'
-import { act } from 'react-test-renderer'
-
-jest.useFakeTimers()
-jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper')
 
 describe('<App />', () => {
   const component = render(<App />)
 
-  it('App', async () => {
-    await act(async () => {})
-  })
+  it('App', async () => {})
 })
