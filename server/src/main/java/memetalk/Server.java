60905f79cfe6732c4c582097aa31e3200a02a83b
Heron Yang <heron.yang.tw@gmail.com>
2020-09-23 21:36:10 -0700

    Enable CORS on server.

--- server/src/main/java/memetalk/Server.java
+++ server/src/main/java/memetalk/Server.java
@@ -2,10 +2,30 @@ package memetalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class Server {
     public static void main(String[] args) {
         SpringApplication.run(Server.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean =
+                new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
