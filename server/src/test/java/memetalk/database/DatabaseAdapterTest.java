4c455814e871a04f61ded59bb010e275d25768da
Heron Yang <heron.yang.tw@gmail.com>
2020-10-09 20:49:53 -0700

    Throw errors in test code directly instead of trying to catch them.

--- server/src/test/java/memetalk/database/DatabaseAdapterTest.java
+++ server/src/test/java/memetalk/database/DatabaseAdapterTest.java
@@ -21,68 +21,48 @@ import org.junit.Test;
  */
 public class DatabaseAdapterTest {
     @BeforeClass
-    public static void setUpBeforeClass() {
+    public static void setUpBeforeClass() throws Exception {
         configReader = new ConfigReader(DeploymentEnvironment.TEST);
         connectToFakeDatabase();
     }
 
     @AfterClass
-    public static void tearDownAfterClass() {
+    public static void tearDownAfterClass() throws Exception {
         disconnectFromFakeDatabase();
     }
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         generateFakeData();
     }
 
-    private static void connectToFakeDatabase() {
-        try {
-            connection =
-                    DriverManager.getConnection(
-                            configReader.getConfig("db-url"),
-                            configReader.getConfig("db-username"),
-                            configReader.getConfig("db-password"));
-        } catch (Exception e) {
-            e.printStackTrace();
-            Assert.fail("Unexcepted exception.");
-        }
+    private static void connectToFakeDatabase() throws Exception {
+        connection =
+                DriverManager.getConnection(
+                        configReader.getConfig("db-url"),
+                        configReader.getConfig("db-username"),
+                        configReader.getConfig("db-password"));
     }
 
-    private static void disconnectFromFakeDatabase() {
-        try {
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Assert.fail("Unexcepted exception.");
-        }
+    private static void disconnectFromFakeDatabase() throws Exception {
+        connection.close();
     }
 
-    private void generateFakeData() {
-        try {
-            Statement statement = connection.createStatement();
-            statement.execute("DROP TABLE meme IF EXISTS;");
-            statement.execute("CREATE TABLE meme (url TEXT);");
-            statement.execute("INSERT into meme (url) VALUES ('http://www.happy.com');");
-            statement.close();
-            connection.commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Assert.fail("Unexcepted exception.");
-        }
+    private void generateFakeData() throws Exception {
+        Statement statement = connection.createStatement();
+        statement.execute("DROP TABLE meme IF EXISTS;");
+        statement.execute("CREATE TABLE meme (url TEXT);");
+        statement.execute("INSERT into meme (url) VALUES ('http://www.happy.com');");
+        statement.close();
+        connection.commit();
     }
 
     @Test
-    public void testSelectSucceed() {
-        try {
-            DatabaseAdapter databaseAdapter = new DatabaseAdapter(configReader);
-            List<Meme> memes = databaseAdapter.getMemes();
-            Assert.assertEquals(memes.size(), 1);
-            Assert.assertEquals(memes.get(0).getUrl(), "http://www.happy.com");
-        } catch (Exception e) {
-            e.printStackTrace();
-            Assert.fail("Unexcepted exception.");
-        }
+    public void testSelectSucceed() throws Exception {
+        DatabaseAdapter databaseAdapter = new DatabaseAdapter(configReader);
+        List<Meme> memes = databaseAdapter.getMemes();
+        Assert.assertEquals(memes.size(), 1);
+        Assert.assertEquals(memes.get(0).getUrl(), "http://www.happy.com");
     }
 
     private static Connection connection = null;
