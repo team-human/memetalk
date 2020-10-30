8b08fdae82af65595ef310ec46339025dc4773d8
Albert <albertchiu@koodata.com>
2020-10-30 14:57:58 +0800

    Updates

---
+++ precache-manifest.be886759170ab86736d5eddd622476d9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d19d3836b3e49664eb1",
+    "url": "/memetalk/static/js/app.35d9c49c.chunk.js"
+  },
+  {
+    "revision": "523d35b82a1059134048",
+    "url": "/memetalk/static/js/runtime~app.fc5bacb8.js"
+  },
+  {
+    "revision": "e2c01e6cd3e6e3d745d3",
+    "url": "/memetalk/static/js/2.823423c0.chunk.js"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/memetalk/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/memetalk/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "6cea0e4084b599cb0e68347e48f9ba0c",
+    "url": "/memetalk/static/media/temp.c424179f.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/memetalk/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/memetalk/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/memetalk/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/memetalk/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/memetalk/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/memetalk/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/memetalk/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/memetalk/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/memetalk/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/memetalk/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "f3f68bf5b7dce8acae55c25f724625dd",
+    "url": "/memetalk/expo-service-worker.js"
+  },
+  {
+    "revision": "d3b383557acb3cc0d176ae7e214e60e5",
+    "url": "/memetalk/index.html"
+  },
+  {
+    "revision": "b2be9560bafdef1417f8b1e7e01b8709",
+    "url": "/memetalk/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/memetalk/serve.json"
+  },
+  {
+    "revision": "4f365ab8a87798c639009f0dd1d5aad2",
+    "url": "/memetalk/static/js/2.823423c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/memetalk/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/memetalk/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/memetalk/favicon.ico"
+  },
+  {
+    "revision": "da48aead943fe6d06b82eb5fa1835992",
+    "url": "/memetalk/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/memetalk/expo-service-worker.js",{scope:"/memetalk/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
