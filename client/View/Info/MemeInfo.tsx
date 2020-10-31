f6c86b1f46248a60a405d52e07caef3971942549
Jason Pan <lipan@microsoft.com>
2020-10-31 15:15:43 -0700

    solve comment

--- client/View/Info/MemeInfo.tsx
+++ client/View/Info/MemeInfo.tsx
@@ -14,7 +14,7 @@ export const MemeInfo = ({
   discussionCount,
   shareCount,
 }: IMemeInfoProps) => {
-  const date = creationTime ? new Date(creationTime) : new Date()
+  const creationDate = creationTime ? new Date(creationTime) : new Date()
   const [discussionNum, setDiscussionNum] = useState(0)
   const [shareNum, setShareNum] = useState(0)
 
@@ -23,32 +23,14 @@ export const MemeInfo = ({
     if (shareCount) setShareNum(shareCount)
   }, [discussionCount, shareCount])
 
-  const dateString = creationTime
-    ? `${(date.getMonth() + 1)
-      .toString()
-      .padStart(2, '0')}/${date
-        .getDate()
-        .toString()
-        .padStart(2, '0')}/${date
-          .getFullYear()
-          .toString()
-          .padStart(4, '0')} ${date
-            .getHours()
-            .toString()
-            .padStart(2, '0')}:${date
-              .getMinutes()
-              .toString()
-              .padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`
-    : ''
-
   return (
     <View style={styles.container}>
       <View style={styles.date}>
-        <Text>{dateString}</Text>
+        <Text>{creationDate.toLocaleString()}</Text>
       </View>
       <View style={styles.number}>
-        <Text>{`Discussion: ${discussionNum} > 1 : ${pluralize("Discussion")} : "Discussion"`}</Text>
-        <Text>{`Share: ${shareNum} > 1 : ${pluralize("Share")} : "Share"`}</Text>
+        <Text>{` ${discussionNum} ${discussionNum > 1 ? pluralize("Discussion") : "Discussion"} `}</Text>
+        <Text>{` ${shareNum} ${shareNum > 1 ? pluralize("Share") : "Share"} `}</Text>
       </View>
     </View>
   )
