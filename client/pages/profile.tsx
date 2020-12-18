58f0c49990f746515624b08bcf7322b1171cdb47
Liang Yu Pan <xxzero12@gmail.com>
2020-12-19 00:08:10 +0800

    clean up codes

--- client/pages/profile.tsx
+++ client/pages/profile.tsx
@@ -1,59 +1,18 @@
 import { useQuery } from '@apollo/client'
 import React, { useState } from 'react'
 import {
+  ActivityIndicator,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native'
 import { FlatGrid } from 'react-native-super-grid'
+import { tagColorList } from '../Constants/Color'
 import { GET_CURRENT_USER, GET_MEMES_BY_AUTHORID } from '../Query/TopicsQuery'
 import { Profile } from '../View/Profile/Profile'
-
-const DATA = [
-    { name: 'TURQUOISE', code: '#1abc9c' },
-    { name: 'EMERALD', code: '#2ecc71' },
-    { name: 'PETER RIVER', code: '#3498db' },
-    { name: 'AMETHYST', code: '#9b59b6' },
-    { name: 'WET ASPHALT', code: '#34495e' },
-    { name: 'GREEN SEA', code: '#16a085' },
-    { name: 'NEPHRITIS', code: '#27ae60' },
-    { name: 'BELIZE HOLE', code: '#2980b9' },
-    { name: 'WISTERIA', code: '#8e44ad' },
-    { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-    { name: 'SUN FLOWER', code: '#f1c40f' },
-    { name: 'CARROT', code: '#e67e22' },
-    { name: 'ALIZARIN', code: '#e74c3c' },
-    { name: 'CLOUDS', code: '#ecf0f1' },
-    { name: 'CONCRETE', code: '#95a5a6' },
-    { name: 'ORANGE', code: '#f39c12' },
-    { name: 'PUMPKIN', code: '#d35400' },
-    { name: 'POMEGRANATE', code: '#c0392b' },
-    { name: 'SILVER', code: '#bdc3c7' },
-    { name: 'ASBESTOS', code: '#7f8c8d' },
-    { name: 'TURQUOISE', code: '#1abc9c' },
-    { name: 'EMERALD', code: '#2ecc71' },
-    { name: 'PETER RIVER', code: '#3498db' },
-    { name: 'AMETHYST', code: '#9b59b6' },
-    { name: 'WET ASPHALT', code: '#34495e' },
-    { name: 'GREEN SEA', code: '#16a085' },
-    { name: 'NEPHRITIS', code: '#27ae60' },
-    { name: 'BELIZE HOLE', code: '#2980b9' },
-    { name: 'WISTERIA', code: '#8e44ad' },
-    { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-    { name: 'SUN FLOWER', code: '#f1c40f' },
-    { name: 'CARROT', code: '#e67e22' },
-    { name: 'ALIZARIN', code: '#e74c3c' },
-    { name: 'CLOUDS', code: '#ecf0f1' },
-    { name: 'CONCRETE', code: '#95a5a6' },
-    { name: 'ORANGE', code: '#f39c12' },
-    { name: 'PUMPKIN', code: '#d35400' },
-    { name: 'POMEGRANATE', code: '#c0392b' },
-    { name: 'SILVER', code: '#bdc3c7' },
-    { name: 'ASBESTOS', code: '#7f8c8d' },
-]
-
 
 export default function ProfileScreen({ navigation }) {
   const [selectedId, setSelectedId] = useState(null)
@@ -69,26 +28,55 @@ export default function ProfileScreen({ navigation }) {
       },
     }
   )
-    console.log(authMemeData);
+
+  const memeLIst = authMemeIsLoading
+    ? []
+    : authMemeData
+    ? authMemeData.memesByAuthorId.map((meme: any) => {
+        return {
+          id: meme.id,
+          tags: meme.tags,
+        }
+      })
+    : []
+
+  const renderItem = ({ item }) => {
+    const bgColor =
+      tagColorList[Math.floor(Math.random() * Math.floor(tagColorList.length))]
+    return (
+      <TouchableOpacity onPress={() => setSelectedId(item.id)}>
+        <View style={[styles.itemContainer, { backgroundColor: bgColor }]}>
+          <Text style={styles.tags}>{item.tags.join(',')}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
 
   return (
     <>
-    <Profile {...currentUserData?.currentUser}/>
-      <SafeAreaView style={styles.container}>
-        <FlatGrid
+      {currentUserIsLoading ? (
+        <View style={[loadingStyles.container, loadingStyles.horizontal]}>
+          <ActivityIndicator size="large" />
+        </View>
+      ) : (
+        <Profile {...currentUserData?.currentUser} />
+      )}
+      {authMemeIsLoading ? (
+        <View style={[loadingStyles.container, loadingStyles.horizontal]}>
+          <ActivityIndicator size="large" />
+        </View>
+      ) : (
+        <SafeAreaView style={styles.container}>
+          <FlatGrid
             itemDimension={100}
-            data={DATA}
+            data={memeLIst}
             style={styles.gridView}
             fixed
             spacing={16}
-            renderItem={({ item }) => (
-                <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
-                    <Text style={styles.itemName}>{item.name}</Text>
-                    <Text style={styles.itemCode}>{item.code}</Text>
-                </View>
-            )}
-        />
-      </SafeAreaView>
+            renderItem={renderItem}
+          />
+        </SafeAreaView>
+      )}
     </>
   )
 }
@@ -97,7 +85,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
   gridView: {
     marginTop: 10,
@@ -109,7 +97,7 @@ const styles = StyleSheet.create({
     padding: 10,
     height: 100,
   },
-  itemName: {
+  tags: {
     fontSize: 16,
     color: '#fff',
     fontWeight: '600',
@@ -118,5 +106,17 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     fontSize: 12,
     color: '#fff',
+  },
+})
+
+const loadingStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
   },
 })
