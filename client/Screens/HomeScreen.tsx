37b7a862a042e4e24f8adb6f8fef483da1fefa05
Albert Chiu <ab71433@hotmail.com>
2021-02-04 23:02:37 +0800

    move all instance outside HomeScreen

--- client/Screens/HomeScreen.tsx
+++ client/Screens/HomeScreen.tsx
@@ -5,36 +5,33 @@ import { ComposeScreen } from "./ComposeScreen";
 import { ProfileScreen } from "./ProfileScreen";
 import { TopicScreen } from "./TopicScreen";
 
+const Tab = createBottomTabNavigator()
+const TopicStack = createStackNavigator()
+const ProfileStack = createStackNavigator()
+const ComposeStack = createStackNavigator()
+const TopicStackScreen = () => {
+    return (
+        <TopicStack.Navigator>
+            <TopicStack.Screen name="Topic" component={TopicScreen} />
+        </TopicStack.Navigator>
+    )
+}
+const ProfileStackScreen = () => {
+    return (
+        <ProfileStack.Navigator>
+            <ProfileStack.Screen name="Profile" component={ProfileScreen} />
+        </ProfileStack.Navigator>
+    )
+}
+const ComposeStackScreen = () => {
+    return (
+        <ComposeStack.Navigator>
+            <ComposeStack.Screen name="Compose" component={ComposeScreen} />
+        </ComposeStack.Navigator>
+    )
+}
+
 export const HomeScreen = () => {
-    const Tab = createBottomTabNavigator()
-
-    const TopicStack = createStackNavigator()
-    const TopicStackScreen = () => {
-        return (
-            <TopicStack.Navigator>
-                <TopicStack.Screen name="Topic" component={TopicScreen} />
-            </TopicStack.Navigator>
-        )
-    }
-
-    const ProfileStack = createStackNavigator()
-    const ProfileStackScreen = () => {
-        return (
-            <ProfileStack.Navigator>
-                <ProfileStack.Screen name="Profile" component={ProfileScreen} />
-            </ProfileStack.Navigator>
-        )
-    }
-
-    const ComposeStack = createStackNavigator()
-    const ComposeStackScreen = () => {
-        return (
-            <ComposeStack.Navigator>
-                <ComposeStack.Screen name="Compose" component={ComposeScreen} />
-            </ComposeStack.Navigator>
-        )
-    }
-
     return (
         <Tab.Navigator>
             <Tab.Screen name="Topic" component={TopicStackScreen} />
